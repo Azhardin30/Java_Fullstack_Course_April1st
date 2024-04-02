7749577c99e4a1a5d6373d1fb624eb7e6c989478
Azhar170 <36988166+Azhar170@users.noreply.github.com>
2024-04-02 13:49:25 +0530

    Initial commit

---
+++ src/main/java/Pgm1.java
@@ -0,0 +1,12 @@
+public class Pgm1 {
+    public static void main(String[] args) {
+        int a = 5, b = 10;
+        if (a < b) {
+            System.out.println("yes a is bigger");
+        }else{
+            System.out.println("no b is bigger");
+        }
+
+
+    }
+}
